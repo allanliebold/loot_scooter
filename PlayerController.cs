53f60de9f18cd8e106a8783b8b3c2b47b5f6cb15
Allan Liebold <allan.liebold@gmail.com>
2018-07-31 06:09:40 -0700

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -5,5 +5,16 @@ public class PlayerController : MonoBehaviour {
   public Rigidbody2D playerBody;
   
   public float moveSpeed;
-      
+  
+  void Start() {
+    playerBody = GetComponent<Rigidbody2D> (); 
+  }
+  
+  void Update() {
+    if(Input.GetAxisRaw("Horizontal") > 0f) {
+      playerBody.velocity = new Vector3(moveSpeed, playerBody.velocity.y, 0f);  
+    } else if(Input.GetAxisRaw("Horizontal") < 0f) {
+      playerBody.velocity = new Vector3(-moveSpeed, playerBody.velocity.y, 0f);
+    }
+  }
 }

656a15665c7dea1ddbdcc17aaa4cd71fec890fcc
Allan Liebold <allan.liebold@gmail.com>
2018-08-01 06:09:52 -0700

    Update Pickup.cs

--- Pickup.cs
+++ Pickup.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 using System.Collections;
 
 public GameObject levelManager;
+public int pointValue;
 
 public class Pickup : MonoBehaviour {
   void onTriggerEnter2D(Collision2D other) {
     if(other.name == "Player) {
+      levelManager.AddToScore(pointValue);
       Destroy(this.gameObject);    
     }
   }

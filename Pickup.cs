d27ef82a9a8f4a5b735e5962c1307274ef10556a
Allan Liebold <allan.liebold@gmail.com>
2018-08-01 06:05:45 -0700

    Update Pickup.cs

--- Pickup.cs
+++ Pickup.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Pickup : MonoBehaviour {
   void onTriggerEnter2D(Collision2D other) {
-    
+    if(other.name == "Player) {
+      Destroy(this.gameObject);    
+    }
   }
 }

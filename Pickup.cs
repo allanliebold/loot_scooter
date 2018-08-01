b2e6f7516351ef916410439fe25142eb3a54f667
Allan Liebold <allan.liebold@gmail.com>
2018-08-01 06:06:58 -0700

    Update Pickup.cs

--- Pickup.cs
+++ Pickup.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections;
 
+public GameObject levelManager;
+
 public class Pickup : MonoBehaviour {
   void onTriggerEnter2D(Collision2D other) {
     if(other.name == "Player) {

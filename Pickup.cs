2716d1ed86a1be94e4ca0169d6f92af26b6d5645
Allan Liebold <allan.liebold@gmail.com>
2018-08-02 06:06:41 -0700

    Update Pickup.cs

--- Pickup.cs
+++ Pickup.cs
@@ -5,6 +5,10 @@ public class Pickup : MonoBehaviour {
   public LevelManager levelManager;
   public int pointValue;
   
+  void Start() {
+    
+  }
+  
   void onTriggerEnter2D(Collision2D other) {
     if(other.name == "Player") {
       levelManager.AddToScore(pointValue);
